f9694a3457a25d16b14a4b7a9a17eb35218e2cd7
Christopher Henkel <c.henkel@online.de>
2015-02-13 00:11:13 +0100

    got rid of mongo

--- server/server.js
+++ server/server.js
@@ -1,26 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
 var cors = require("cors");
-
-
-mongoose.connect('mongodb://admin:88chr88chrCwKdA@localhost/goebu');
-
-var db = mongoose.connection;
-db.on('error', function (error) {
-    console.log(error);
-});
-db.once('open', function (callback) {
-    console.log('connection opened');
-});
-
-//var personSchema = {
-//    firstName: String,
-//    lastName: String,
-//    email: String
-//};
-
-//var Person = mongoose.model('Person', personSchema, 'people');
 
 var app = express();
 
@@ -39,24 +19,5 @@ var server = app.listen(3001, function () {
     console.log('Example app listening at http://%s:%s', host, port);
 
 });
-
-//app.route('/people')
-//    .get(function (req, res) {
-//        Person.find().select("firstName").limit(10).exec(function (err, doc) {
-//            res.send(doc);
-//        })
-//    })
-//
-//    .post(function (req, res) {
-//        Person.update({_id: req.body._id}, {firstName: req.body.firstName}, function (err) {
-//            res.send(req.body)
-//        })
-//    });
-//
-//app.get('/people/:id', function (req, res) {
-//    Person.findById(req.params.id, function (err, doc) {
-//        res.send(doc);
-//    })
-//});
 
 app.listen(3000);
