52f0d3899c8878a01f9f1ba616eea943324e5623
Christopher Henkel <c.henkel@online.de>
2015-06-18 01:14:19 +0200

    schicker

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -261,7 +261,7 @@ angular
 
     })
 
-    .directive('headerShrink', function ($document) {
+    .directive('headerShrinkTwitterStyle', function ($document) {
         // var shrink = function(header, content, amt, max) {
         //   amt = Math.min(250, amt);
         //   ionic.requestAnimationFrame(function() {
@@ -273,7 +273,8 @@ angular
             restrict: 'A',
             link: function ($scope, $element, $attr) {
                 var resizeFactor, scrollFactor, blurFactor;
-                var header = $document[0].body.querySelector('#map_canvas');
+                //var header = $document[0].body.querySelector('.map-wrapper');
+                var header2 = $document[0].body.querySelector('#map_canvas');
 
                 $element.bind('scroll', function (e) {
                     if (e.detail.scrollTop >= 0) {
@@ -281,13 +282,16 @@ angular
                         // shrinkAmt = headerHeight - Math.max(0, (starty + headerHeight) - e.detail.scrollTop);
                         // shrink(header, $element[0], shrinkAmt, headerHeight);
                         scrollFactor = e.detail.scrollTop / 2;
-                        header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, +' + scrollFactor + 'px, 0)';
+                        //header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, +' + scrollFactor + 'px, 0)';
+                        header2.style[ionic.CSS.TRANSFORM] = 'translate3d(0, +' + scrollFactor + 'px, 0)';
                     } else {
                         // shrink(header, $element[0], 0, headerHeight);
                         resizeFactor = -e.detail.scrollTop / 100 + 0.99;
                         blurFactor = -e.detail.scrollTop / 10;
-                        header.style[ionic.CSS.TRANSFORM] = 'scale(' + resizeFactor + ',' + resizeFactor + ')';
-                        header.style.webkitFilter = 'blur(' + blurFactor + 'px)';
+                        //header.style[ionic.CSS.TRANSFORM] = 'scale(' + resizeFactor + ',' + resizeFactor + ')';
+                        header2.style[ionic.CSS.TRANSFORM] = 'scale(' + resizeFactor + ',' + resizeFactor + ')';
+                        //header.style.webkitFilter = 'blur(' + blurFactor + 'px)';
+                        header2.style.webkitFilter = 'blur(' + blurFactor + 'px)';
                     }
                 });
             }
--- www/js/map-controller.js
+++ www/js/map-controller.js
@@ -1,7 +1,7 @@
 "use strict";
 angular.module("goebu.controllers")
 
-    .controller('MapCtrl', function ($scope, $ionicLoading, $ionicSlideBoxDelegate) {
+    .controller('MapCtrl', function ($scope, $ionicLoading, $ionicSlideBoxDelegate, $cordovaGeolocation) {
 
         // GLOBAL variables
         var rendererOptions = {
@@ -11,7 +11,7 @@ angular.module("goebu.controllers")
         var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
         var directionsService = new google.maps.DirectionsService();
 
-        var originMarker, destinationMarker;
+        var originMarker, destinationMarker, userLocationMarker;
         var map;
 
         var currentBusLines;
@@ -37,13 +37,21 @@ angular.module("goebu.controllers")
 
             var mapOptions = {
                 center: myLatlng,
-                zoom: 16,
+                zoom: 15,
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 disableDefaultUI: true
 
             };
             map = new google.maps.Map(document.getElementById("map_canvas"),
                 mapOptions);
+
+            var currentLocationControlDiv = document.createElement('div');
+            currentLocationControlDiv.style.paddingRight = '10px';
+            currentLocationControlDiv.style.paddingTop = '10px';
+            var currentLocationControl = new CurrentLocation(currentLocationControlDiv, map);
+
+            currentLocationControlDiv.index = 1;
+            map.controls[google.maps.ControlPosition.TOP_RIGHT].push(currentLocationControlDiv);
 
             directionsDisplay.setMap(map);
             directionsDisplay.setPanel(document.getElementById('directions-panel'));
@@ -57,24 +65,29 @@ angular.module("goebu.controllers")
                 position: new google.maps.LatLng(51.547750, 9.945073),
                 map: map,
                 draggable: true,
+                optimized: false,
+                zIndex: -10,
                 icon: 'img/origin.png'
             });
 
             destinationMarker = new google.maps.Marker({
-                position: new google.maps.LatLng(51.530426, 9.948249),
+                //position: new google.maps.LatLng(51.530426, 9.948249),
                 map: map,
                 draggable: true,
+                optimized: false,
+                zIndex: -1,
                 icon: 'img/destination.png'
             });
 
-            //google.maps.event.addListener(map, 'click', function (event) {
-            //    placeMarker(event.latLng);
-            //});
+            google.maps.event.addListener(map, 'click', function (event) {
+                placeMarker(event.latLng);
+            });
 
             google.maps.event.addListener(originMarker, 'dragend', function () {
                 //alert('originMarker dragged');
                 calcRoute();
             });
+
             google.maps.event.addListener(destinationMarker, 'dragend', function () {
                 //alert('destinationMarker dragged');
                 calcRoute();
@@ -91,27 +104,25 @@ angular.module("goebu.controllers")
                 directionsDisplayUpdated();
 
             });
+            getCurrentLocationStart();
 
-            calcRoute();
+            $scope.title = "Wo möchten Sie hin?";
 
             $scope.map = map;
         }
 
         google.maps.event.addDomListener(window, 'load', initialize);
 
-        //function placeMarker(location) {
-        //    var marker = new google.maps.Marker({
-        //        position: location,
-        //        draggable: true,
-        //        map: map
-        //    });
-        //
-        //    map.setCenter(location);
-        //}
+
+
+        function placeMarker(location) {
+            destinationMarker.setPosition(location);
+            calcRoute();
+        }
 
         function directionsDisplayUpdated() {
             var routeIndex = directionsDisplay && directionsDisplay.hasOwnProperty('routeIndex') ? directionsDisplay.routeIndex : null;
-            if (previousRouteIndex !== routeIndex){
+            if (previousRouteIndex !== routeIndex) {
                 findBusLines(directionsDisplay);
             }
             previousRouteIndex = routeIndex;
@@ -170,7 +181,7 @@ angular.module("goebu.controllers")
             directionsService.route(request, function (response, status) {
                 if (status === google.maps.DirectionsStatus.OK) {
                     console.log(response, "<-- response");
-                    
+
                     directionsDisplay.setDirections(response);
                 }
             });
@@ -206,4 +217,144 @@ angular.module("goebu.controllers")
         $scope.clickTest = function () {
             alert('Example of infowindow with ng-click');
         };
+
+        /* Aktuelle Position
+         //
+         //
+         */
+        var watch;
+
+        function getCurrentLocationStart() {
+            var posOptions = {timeout: 10000, enableHighAccuracy: false};
+            $cordovaGeolocation
+                .getCurrentPosition(posOptions)
+                .then(function (position) {
+
+                    var lat = position.coords.latitude;
+                    var long = position.coords.longitude;
+                    setUserLocationMarker(lat, long);
+                }, function (err) {
+                    console.error(err, "!!! error: getCurrentPosition");
+                });
+
+            var watchOptions = {
+                frequency: 1000,
+                timeout: 6000,
+                enableHighAccuracy: false // may cause errors if true
+            };
+
+            watch = $cordovaGeolocation.watchPosition(watchOptions);
+            watch.then(
+                null,
+                function (err) {
+                    console.error(err, "!!! error: watchPosition");
+
+                },
+                function (position) {
+                    var lat = position.coords.latitude;
+                    var long = position.coords.longitude;
+                    setUserLocationMarker(lat, long);
+                });
+
+            //watch.clearWatch();
+            //// OR
+            //$cordovaGeolocation.clearWatch(watch)
+            //    .then(function (result) {
+            //        console.log(result, "<-- clearWatch result, ");
+            //    }, function (error) {
+            //        // error
+            //    });
+        }
+
+        function setUserLocationMarker(lat, lng) {
+            if (!userLocationMarker) {
+                originMarker.setPosition(new google.maps.LatLng(lat, lng));
+                map.setCenter(originMarker.position);
+                var userLocationIcon = new google.maps.MarkerImage(
+                    "img/user-location-disambig.png",
+                    new google.maps.Size(36, 36),
+                    new google.maps.Point(0, 0),
+                    new google.maps.Point(18, 18),
+                    new google.maps.Size(36, 36)
+                );
+
+                userLocationMarker = new google.maps.Marker({
+                    position: new google.maps.LatLng(lat, lng),
+                    map: map,
+                    draggable: false,
+                    icon: userLocationIcon,
+                    optimized: false,
+                    zIndex: -99
+                });
+                //when the map zoom changes,
+                // resize the icon based on the zoom level
+                // so the marker covers the same geographic area
+                google.maps.event.addListener(map, 'zoom_changed', function () {
+                    var minPixelSize = 36; //the size of the icon at zoom level 0
+                    var maxPixelSize = 150; //restricts the maximum size of the icon, otherwise the browser will choke at higher zoom levels trying to scale an image to millions of pixels
+
+                    var zoom = map.getZoom();
+                    var relativePixelSize = Math.round(Math.pow(1.3, zoom)); // use 2 to the power
+                    // of current zoom to calculate relative pixel size.  Base of exponent is 2 because relative size should double every time you zoom in
+
+                    //restrict the maximum size of the icon
+                    if (relativePixelSize > maxPixelSize) {
+                        relativePixelSize = maxPixelSize;
+                    }
+                    if (relativePixelSize < minPixelSize) {
+                        relativePixelSize = minPixelSize;
+                    }
+
+                    //change the size of the icon
+                    userLocationMarker.setIcon(
+                        new google.maps.MarkerImage(
+                            userLocationMarker.getIcon().url, //marker's same icon graphic
+                            null,
+                            new google.maps.Point(0, 0),
+                            new google.maps.Point(relativePixelSize / 2, relativePixelSize / 2),
+                            new google.maps.Size(relativePixelSize, relativePixelSize) //changes the scale
+                        )
+                    );
+
+                });
+            } else {
+                userLocationMarker.setPosition(new google.maps.LatLng(lat, lng));
+            }
+        }
+
+        function CurrentLocation(controlDiv, map) {
+
+            // Set CSS for the control border
+            var controlUI = document.createElement('div');
+            controlUI.style.backgroundColor = 'rgba(255, 255, 255, 0.55)';
+            //controlUI.style.border = '2px solid #fff';
+            controlUI.style.borderRadius = '34px';
+            controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+            controlUI.style.cursor = 'pointer';
+            controlUI.style.marginBottom = '22px';
+            controlUI.style.textAlign = 'center';
+            controlUI.title = 'Click to recenter the map';
+            controlUI.style.width = '44px';
+            controlUI.style.height = '44px';
+
+            controlDiv.appendChild(controlUI);
+
+            // Set CSS for the control interior
+            var controlText = document.createElement('div');
+            controlText.style.color = 'rgb(25,25,25)';
+            controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+            controlText.style.fontSize = '16px';
+            controlText.style.lineHeight = '44px';
+            controlText.style.paddingLeft = '5px';
+            controlText.style.paddingRight = '5px';
+            controlText.innerHTML = '<i class="icon ion-pinpoint"></i>';
+            controlUI.appendChild(controlText);
+
+            // Setup the click event listeners: simply set the map to
+            // Chicago
+            google.maps.event.addDomListener(controlUI, 'click', function () {
+                map.setCenter(userLocationMarker.position);
+            });
+
+        }
     });
