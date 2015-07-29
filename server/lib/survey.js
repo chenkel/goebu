f406d674461c01a9b9d4ae9abefb0a0a6cd0e34e
Christopher Henkel <c.henkel@online.de>
2015-07-29 20:45:23 +0200

    Survey_ger ohne Kontrollvariablen

--- server/lib/survey.js
+++ server/lib/survey.js
@@ -1,111 +1,90 @@
 "use strict";
 var moment = require("moment");
 
+var likertScale = [
+    'trifft zu',
+    'trifft eher zu',
+    'teils-teils',
+    'trifft eher nicht zu',
+    'trifft nicht zu'
+];
+
 var surveyQuestions = [
     {},
     {
-        id: 1,
-        title: 'Mini-Umfrage',
-        description: 'Schon fertig ? ',
+        id: 10,
         questions: [
             {
-                id: 1001,
-                text: 'How many times do you use the app on a typical day?',
+                id: 1010,
+                text: 'Wie oft haben Sie die App letzte Woche geöffnet?',
                 options: []
             },
             {
-                id: 1002,
-                text: 'How many routes are you looking up on a typical day?', options: []
+                id: 1020,
+                text: 'Wie viele Routen haben Sie in der letzten Woche nachgeschaut?', options: []
             },
             {
-                id: 1003,
-                text: 'For how long (in minutes) are using the app on a typical day?',
-                options: []
-            },
-            {
-                id: 3004,
-                text: 'How often are you looking up your typically used route?',
+                id: 1030,
+                text: 'Wie viele Minuten haben Sie die App letzte Woche insgesamt genutzt?',
                 options: []
             }
         ]
     },
     {
-        id: 2,
-        title: 'Mini-Umfrage für Simon',
-        description: 'Schon fertig, Dr. Trang ? ',
+        id: 20,
         questions: [
             {
-                id: 2001,
-                text: 'How many times do you use the app on a typical day?',
-                options: []
-            },
-            {
-                id: 2002,
-                text: 'How many routes are you looking up on a typical day?', options: []
-            },
-            {
-                id: 2003,
-                text: 'How often do you use the app?',
+                id: 2010,
+                text: 'Wie oft haben Sie die App letzte Woche geöffnet?',
                 options: [
-                    'More than once a day',
-                    'Once a day',
-                    'Several times a week',
-                    'Several times a month',
-                    'Never'
+                    'Mehr als einmal täglich',
+                    'Einmal pro Tag',
+                    'Mehrmals pro Woche',
+                    'Einmal pro Woche',
+                    'Niemals'
                 ]
             },
             {
-                id: 2004,
-                text: '"I often look up my typically used route"',
+                id: 2020,
+                text: 'Wie viele Routen haben Sie in der letzten Woche nachgeschaut?',
                 options: [
-                    'Strongly agree',
-                    'Agree',
-                    'Neither agree nor disagree',
-                    'Disagree',
-                    'Strongly disagree'
+                    'Mehr als 15',
+                    '7 - 15',
+                    '4 - 7',
+                    '1 - 3',
+                    '0'
+                ]
+            },
+            {
+                id: 2030,
+                text: 'Wie viele Minuten haben Sie die App letzte Woche insgesamt genutzt?',
+                options: [
+                    'Länger als 20 Minuten',
+                    '14 - 20',
+                    '7 - 14',
+                    '1 - 7',
+                    '0 Minuten'
                 ]
             }
         ]
     },
     {
-        id: 3,
-        title: 'Mini-Umfrage',
-        description: 'Buhaa ? ',
+        id: 30,
         questions: [
             {
-                id: 3001,
-                text: 'How many times do you use the app on a typical day?',
-                options: [
-                    'AAAAAAA',
-                    'ZZZZZZZ'
-                ]
+                id: 3010,
+                text: '“Letzte Woche habe ich die App jeden Tag benutzt.”',
+                options: likertScale
             },
             {
-                id: 3002,
-                text: 'How many routes are you looking up on a typical day?',
-                options: []
+                id: 3020,
+                text: '“Letzte Woche habe ich viele Busrouten nachgeschaut.”',
+                options: likertScale
             },
             {
-                id: 3003,
-                text: 'For how long (in minutes) are using the app on a typical day?',
-                options: [
-                    'More than once a day',
-                    'Once a day',
-                    'Several times a week',
-                    'Several times a month',
-                    'Never'
-                ]
-            },
-            {
-                id: 3004,
-                text: 'How often are you looking up your typically used route?',
-                options: [
-                    'Strongly agree',
-                    'Agree',
-                    'Neither agree nor disagree',
-                    'Disagree',
-                    'Strongly disagree'
-                ]
+                id: 3030,
+                text: '“Letzte Woche habe ich die App lange genutzt.“',
+                options: likertScale
             }
         ]
     }
@@ -118,12 +97,14 @@ module.exports = {
         var duration = moment.duration(now.diff(firstTimeOpened));
         var daysSince = duration.asDays();
 
-        var surveyWeek = Math.floor(daysSince / 6);
+        var surveyWeek = Math.floor(daysSince / 7);
+        console.log(surveyWeek, "<-- surveyWeek");
+
         if (surveyWeek > 3) {
             surveyWeek = 3;
         }
         //console.log(group_id, "<-- group_id");
-        //console.log(surveyWeek, "<-- surveyWeek");
+
 
         //var surveyPlan = {
         //    1: [0, 1, 2, 3],
@@ -140,12 +121,14 @@ module.exports = {
         //console.log(surveyQuestions[2], "<-- surveyQuestions");
 
         var chosenSurvey = surveyQuestions[surveyPlan[group_id][surveyWeek]];
-        if (chosenSurvey){
+        if (chosenSurvey.hasOwnProperty('id')) {
             chosenSurvey.disclaimer1 = "Die Umfrage wird im Rahmen eines Forschungsprojekts der Universität Göttingen durchgeführt.";
             chosenSurvey.disclaimer2 = "Persönliche Daten werden weder abgefragt noch gespeichert.";
             chosenSurvey.disclaimer3 = "Alle Angaben werden verschlüsselt und anonymisiert übertragen und in keinem Fall an Dritte weitergegeben.";
+            chosenSurvey.title = 'Mini-Umfrage';
+            chosenSurvey.description = 'Umfrage abgeschlossen ?';
         }
-            cb(null, chosenSurvey);
+        cb(null, chosenSurvey);
     }
 };
 
